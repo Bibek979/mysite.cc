9d531087a7ab3b0e588676935d336439b680b375
Bibek979 <bibekmeananda@gmail.com>
2023-01-24 10:46:05 +0530

    Added all the contents and contact form fixed RWD issues

--- index.js
+++ index.js
@@ -8,4 +8,21 @@ $(document).ready(function(){
         $("#will-disappear").fadeOut();
         $("#first-name").animate({fontSize: '4.5rem'});
     })
+    // code for RWD mobile menu
+    var menuOpen = false;
+    $("#menu-icon").click(function() {
+        if(menuOpen === false){
+            menuOpen = true;
+            console.log("Wrking")
+            $("#menu-icon-img").attr("class", "menu-icon-img-cls")
+            $("#menu-close-icon").attr("class", "menu-close-icon-active")
+            $("#sub-menu-id").attr("class", "sub-menu-active")
+        }
+        else{
+            menuOpen = false;
+            $("#menu-icon-img").attr("class", "menu-icon-img-class")
+            $("#menu-close-icon").attr("class", "menu-close-icon-disabled")
+            $("#sub-menu-id").attr("class", "sub-menu")
+        }
+    })
 })
